70c33127ba4fa25dd5e43eba6284a6c7960bc0b6
eugenio-perdomo <scnhp.0894@gmail.com>
2021-03-29 17:43:05 -0300

    Uso de .h

--- classes/headers/Arribo.h
+++ classes/headers/Arribo.h
@@ -1,12 +1,32 @@
 #ifndef ARRIBO_H
 #define ARRIBO_H
-
+/* Aca van las funciones que van a usar atributos*/
 #include "../../datatypes/headers/DtFecha.h"
+#include "../../datatypes/headers/DtBarco.h"
 
-class Arribo {
+class Arribo
+{
+private:
+    DtBarco barcoQueArriba;
+    DtFecha fechaDeArribo;
+    float cantidadDespachada; // Puede ser negativa
+
 public:
-  Arribo(float carga, DtFecha fecha);
-  DtFecha getFecha();
+    // Constructor
+    DtArribo(DtBarco barcoQueArriba, DtFecha fechaDeArribo, float cantidadDespachada);
+
+    // Getters
+    DtBarco getBarcoQueArriba();
+    DtFecha getFechaDeArribo();
+    float getCantidadDespachada();
+
+    // Setters
+    void setBarcoQueArriba(DtBarco barcoQueArriba);
+    void setFechaDeArribo(DtFecha getFechaDeArribo);
+    void setCantidadDespachada(float cantidadDespachada);
+// void funcion1() { } // el main llama a estas
+// void funcion2() { }
+    virtual ~DtArribo();
 };
 
 #endif /* ARRIBO_H */
--- datatypes/headers/DtArribo.h
+++ datatypes/headers/DtArribo.h
@@ -1,20 +1,31 @@
 #ifndef DTARRIBO_H
 #define DTARRIBO_H
+/* Esto es nada mas para estructura */
+#include "../../datatypes/headers/DtFecha.h"
+#include "../../datatypes/headers/DtBarco.h"
 
-class DtArribo{
-    private:
-        DtBarco barcoQueArriba;
-        DtFecha fechaDeArribo;
-        float cantidadDespachada;
+class DtArribo
+{
+private:
+    DtBarco barcoQueArriba;
+    DtFecha fechaDeArribo;
+    float cantidadDespachada;
 
-    public:
-        DtArribo(DtBarco barcoQueArriba, DtFecha fechaDeArribo, float cantidadDespachada);
+public:
+    // Constructor
+    DtArribo(DtBarco barcoQueArriba, DtFecha fechaDeArribo, float cantidadDespachada);
 
-        DtBarco barcoQueArriba() const;
-        DtFecha fechaDeArribo() const;
-        float cantidadDespachada() const;
+    // Getters
+    DtBarco getBarcoQueArriba();
+    DtFecha getFechaDeArribo();
+    float getCantidadDespachada();
 
-        virtual ~DtArribo();
+    // Setters
+    void setBarcoQueArriba(DtBarco barcoQueArriba);
+    void setFechaDeArribo(DtFecha getFechaDeArribo);
+    void setCantidadDespachada(float cantidadDespachada);
+
+    virtual ~DtArribo();
 };
 
-#endif /* ARRIBO_H */
+#endif /* DTARRIBO_H */
